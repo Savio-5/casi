1d2b19c53323c54ffb922cd5260f91c70ec0b4ea
Savio Silveira <s3.silveira@gmail.com>
2024-07-23 19:59:23 +0530

    basic page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className= "flex min-h-screen flex-col items-center text-center justify-around p-24" >
+      <div className="max-w-md" >
+        <img className="mask mask-squircle" src = "/casi_pizza.jpg" alt = "Casi Pizza" />
+        <h1 className="text-5xl font-bold mt-6" > Bye Bye Pilu! </h1>
+        <p className = "py-6" > Meow </p>
+      </div>
+    </main>
+  );
+}
